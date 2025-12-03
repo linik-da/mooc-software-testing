fd48d7827ef5193c9ffdd380fca4635655fe2739
Linda <60820284+linik-da@users.noreply.github.com>
2025-12-03 21:50:22 +0100

    Adds new Testcases to CeasarShiftCipherTest

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,22 @@
 package tudelft.caesarshift;
 
-public class CaesarShiftCipherTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
+public class CaesarShiftCipherTest {
+    @ParameterizedTest(name = "originalString= {0}, shift = {1}, result = {2}")
+    @CsvSource({"'abc', 3, 'def'", "'a', 3, 'd'", "'a', 0, 'a'"})
+    public void canBeShifted(String string, int shift, String expectedResult){
+        CaesarShiftCipher ceasarShiftCipher = new CaesarShiftCipher();
+        String result = ceasarShiftCipher.CaesarShiftCipher(string, shift);
+        Assertions.assertEquals(expectedResult,result);
+    }
+    @ParameterizedTest(name = "originalString= {0}, shift = {1}, result = {2}")
+    @CsvSource({"'ABC', 3, 'invalid'"})
+    public void canNotBeShifted(String string, int shift, String expectedResult){
+        CaesarShiftCipher ceasarShiftCipher = new CaesarShiftCipher();
+        String result = ceasarShiftCipher.CaesarShiftCipher(string, shift);
+        Assertions.assertEquals(expectedResult,result);
+    }
 }
