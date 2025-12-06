f4944acf377c1ccf815ab1c15bc39a279aa5119a
Linda <60820284+linik-da@users.noreply.github.com>
2025-12-06 20:22:41 +0100

    Adds testcases for condition coverage

--- src/test/java/tudelft/blackjack/BlackJackLineCoverageTest.java
+++ src/test/java/tudelft/blackjack/BlackJackLineCoverageTest.java
@@ -11,9 +11,12 @@ public class BlackJackLineCoverageTest {
         Assertions.assertEquals(0, result);
     }
 
+
     @Test
     public void leftPlayerWins() {
         int result = new BlackJack().play(10, 9);
         Assertions.assertEquals(10, result);
     }
+
+
 }
--- src/test/java/tudelft/countletters/CountLettersTest.java
+++ src/test/java/tudelft/countletters/CountLettersTest.java
@@ -2,13 +2,22 @@ package tudelft.countletters;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class CountLettersTest {
 
+    @ParameterizedTest
+    @CsvSource({"'cats|dogs',2","'car|door',2"})
+    public void multipleMatchingWords(String string,int result) {
+        int words = new CountLetters().count(string);
+        Assertions.assertEquals(result, words);
+    }
+
     @Test
-    public void multipleMatchingWords() {
-        int words = new CountLetters().count("cats|dogs");
-        Assertions.assertEquals(2, words);
+    public void noMatchingWords(){
+        int words = new CountLetters().count("boast|toast");
+        Assertions.assertEquals(0,words);
     }
 
     @Test
@@ -16,5 +25,6 @@ public class CountLettersTest {
         int words = new CountLetters().count("cats|dog");
         Assertions.assertEquals(1, words);
     }
+
 
 }
